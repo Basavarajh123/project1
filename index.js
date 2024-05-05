c0083e0e2f8f5e0fde7c1ede00c885c3f0a0a46f
Basavarajh123 <basavarajh556@gmail.com>
2024-05-05 13:19:52 +0530

    solution

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express= require("express");
 const {open}= require("sqlite");
 const sqlite3 = require("sqlite3");
-const  cors= require("cors");
-const bcryp = require("bcrypt");
+const axios = require('axios');
+
 const path = require('path');
 
 
-const dbPath = path.join(__dirname,"User.db");
+const dbPath = path.join(__dirname,"transactions.db");
 
 const app= express();
-app.use(cors());
+
 
 app.use(express.json());
 
@@ -36,63 +36,96 @@ const initializationDbAndServer =async()=>{
 
 initializationDbAndServer()
 
-app.get('/',(request,response)=>{
-    response.send('Welcome Backend App');
+
+
+const fetchAndInsert = async () => {
+  const response = await axios.get(
+    "https://s3.amazonaws.com/roxiler.com/product_transaction.json"
+  );
+  const data = response.data;
+  
+
+  for (let item of data) {
+    const queryData = `SELECT id FROM transactions WHERE id = ${item.id}`;
+    const existingData = await database.get(queryData);
+    if (existingData === undefined) {
+      const query = `
+   INSERT INTO transactions (id, title, price, description, category, image, sold, dateOfSale) 
+   VALUES (
+       ${item.id},
+       '${item.title.replace(/'/g, "''")}',
+       ${item.price},
+       '${item.description.replace(/'/g, "''")}',
+       '${item.category.replace(/'/g, "''")}',
+       '${item.image.replace(/'/g, "''")}',
+       ${item.sold},
+       '${item.dateOfSale.replace(/'/g, "''")}'
+   );
+`; /*The .replace(/'/g, "''") in the SQL query helps prevent SQL injection attacks by escaping single quotes.*/
+
+      await database.run(query);
+    }
+  }
+  console.log("Transactions added");
+};
+
+fetchAndInsert();
+
+
+app.get('/tasks/', async(req,res)=>{
+
+  const getAllTasksQuery= `SELECT * FROM transactions`;
+  const transactionsData= await database.all(getAllTasksQuery)
+  res.send(transactionsData);
+
+})
+app.get('/stats/:month/',async(req,res)=>{
+  const {month} = req.params;
+  const getStatsQuery= `SELECT 
+                              SUM(
+                                CASE WHEN(sold >0) THEN sold * price END
+                              ) AS TotalSale,
+                                SUM(sold) AS TotalSoldItems,
+                                COUNT(*) AS TotalItems
+
+                               FROM 
+                               transactions 
+                               WHERE   CAST(strftime('%m', dateOfSale) AS INTEGER) = ${month}`
+
+                               const data = await database.get(getStatsQuery)
+                               res.send(data)
 })
 
-app.get('/user/',async(request,response)=>{
-    const sqlQuery=`SELECT * FROM User`
-    const data =await database.all(sqlQuery);
-    response.send(data);
+
+app.get('/transactions/barchart/:month/',async(req,res)=>{
+  const {month}= req.params;
+  const sqlQuery= `SELECT COUNT( CASE WHEN (price <= 100 and price >= 0) THEN 1 END) AS range_1_to_100,
+                    COUNT( CASE WHEN (price <= 200 and price > 100) THEN 1 END) AS range_101_to_200,
+                    COUNT( CASE WHEN (price <= 300 and price > 200) THEN 1 END) AS range_201_to_300,
+                    COUNT( CASE WHEN (price <= 400 and price > 300) THEN 1 END) AS range_301_to_400,
+                    COUNT( CASE WHEN (price <= 500 and price > 400) THEN 1 END) AS range_401_to_500,
+                    COUNT( CASE WHEN (price <= 600 and price > 500) THEN 1 END) AS range_501_to_600,
+                    COUNT( CASE WHEN (price <= 700 and price > 600) THEN 1 END) AS range_601_to_700,
+                    COUNT( CASE WHEN (price <= 800 and price > 700) THEN 1 END) AS range_701_to_800,
+                    COUNT( CASE WHEN (price <= 900 and price > 800) THEN 1 END) AS range_801_to_900,
+                    COUNT( CASE WHEN ( price > 900) THEN 1 END) AS range_901_and_above
+
+  
+  
+  
+  
+  FROM transactions WHERE CAST(strftime('%m', dateOfSale) AS INTEGER) = ${month}`;
+  const data = await database.all(sqlQuery);
+  res.send(data)
 })
 
+app.get('/transactions/piechart/:categoryId/:month/',async(req,res)=>{
+  const {categoryId,month}= req.params;
+  const sqlQueryForPieChart=`SELECT COUNT(*) AS Number_Of_Items FROM transactions WHERE category LIKE "${categoryId}" AND CAST(strftime('%m', dateOfSale) AS INTEGER) = ${month};`;
 
+const data = await database.all(sqlQueryForPieChart);
+res.send(data)
+})
 
-
-
-app.post("/users/", async (request, response) => {
-    const { name, email,password} = request.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const selectUserQuery = `SELECT * FROM User WHERE email = '${email}'`;
-    const dbUser = await database.get(selectUserQuery);
-    if (dbUser === undefined) {
-      const createUserQuery = `
-        INSERT INTO 
-          User (name,email, password) 
-        VALUES 
-          (
-            
-            '${name}',
-            '${email}',
-            '${hashedPassword}'
-            
-          )`;
-      const dbResponse = await database.run(createUserQuery);
-      const newUserId = dbResponse.lastID;
-      response.send(`Created new user with ${newUserId}`);
-    } else {
-      response.status = 400;
-      response.send("User already exists");
-    }
-  });
-
-
-app.post("/login", async (request, response) => {
-    const { email, password } = request.body;
-    const selectUserQuery = `SELECT * FROM User WHERE email = '${email}'`;
-    const dbUser = await database.get(selectUserQuery);
-    if (dbUser === undefined) {
-      response.status(400);
-      response.send("Invalid User");
-    } else {
-      const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
-      if (isPasswordMatched === true) {
-        response.send("Login Success!");
-      } else {
-        response.status(400);
-        response.send("Invalid Password");
-      }
-    }
-  });
 
 module.exports = app;
---
+++ sqltable.sql
@@ -0,0 +1,3 @@
+-- CREATE TABLE transactions (id INTEGER, title VARCHAR(30), price , description VARCHAR(30), category VARCHAR(30), image VARCHAR(30), sold INTEGER, dateOfSale VARCHAR(30))
+
+SELECT * FROM transactions
